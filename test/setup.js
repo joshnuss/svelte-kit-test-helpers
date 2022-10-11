59ac8eff4d684eb6cdcc177def1aaf07d61eb783
Josh Nussbaum <joshnuss@gmail.com>
2022-10-11 10:02:21 -0400

    Adds FormData global

---
+++ test/setup.js
@@ -0,0 +1,13 @@
+global.FormData = class {
+  constructor() {
+    this.data = new Map()
+  }
+
+  append(key, value) {
+    this.data.set(key, value)
+  }
+
+  get(key) {
+    return this.data.get(key)
+  }
+}
--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    setupFiles: ['./test/setup.js']
   }
 })
