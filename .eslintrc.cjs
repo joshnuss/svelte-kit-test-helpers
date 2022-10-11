49c35b604d37e1c0ccd356ebcc9e100625ce8a60
Josh Nussbaum <joshnuss@gmail.com>
2022-10-11 08:52:37 -0400

    Adds prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  }
+}
